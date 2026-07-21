fe030f809bf403e4a521a453358cf83c4aca8ea0
Jianbao Huang <51468896+Bill845514379@users.noreply.github.com>
2026-07-21 16:19:55 +0800

    Revert "[Feature] Add ascendC op sparse_attention_score to support minimax m3"

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -133,7 +133,6 @@ elif [[ "$SOC_VERSION" =~ ^ascend910b ]]; then
         "chunk_gated_delta_rule_fwd_h"
         "store_kv_block"
         "store_kv_block_metadata"
-        "sparse_attention_score"
     )
 
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
@@ -185,7 +184,6 @@ elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
         "chunk_gated_delta_rule_fwd_h"
         "store_kv_block"
         "store_kv_block_metadata"
-        "sparse_attention_score"
     )
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
     SOC_ARG="ascend910_93"
@@ -220,7 +218,6 @@ elif [[ "$SOC_VERSION" =~ ^ascend950 ]]; then
         "sparse_attention_score"
         "store_kv_block"
         "store_kv_block_metadata"
-        "sparse_attention_score"
     )
 
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
